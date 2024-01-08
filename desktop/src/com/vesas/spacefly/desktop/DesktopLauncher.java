3ac951f39b4b17e385e05019a64dbdc4f1765c3d
Vesa <42914624+vesas@users.noreply.github.com>
2024-01-08 19:36:45 +0200

    builds updated, lwjgl to lwjgl3

--- desktop/src/com/vesas/spacefly/desktop/DesktopLauncher.java
+++ desktop/src/com/vesas/spacefly/desktop/DesktopLauncher.java
@@ -1,31 +1,30 @@
 package com.vesas.spacefly.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.vesas.spacefly.SpaceflyGame;
 
 public class DesktopLauncher {
 
 	
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		
-		config.title = "spacefly";
-		config.fullscreen = false;
-		config.vSyncEnabled = false;
-		config.foregroundFPS = 60;
+		config.setTitle("spacefly");
+		// config.fullscreen = false;
+		// config.vSyncEnabled = false;
+		// config.foregroundFPS = 60;
 		// config.width = 1100;
 		// config.height = 650;
 		// config.width = 640*6;
+		// config.height = 480*4;aaaaaa
+		// config.width = 640*4;
 		// config.height = 480*4;
-		config.width = 640*4;
-		config.height = 480*4;
-		// config.width = 640*2;
-		// config.height = 480*2;
+		config.setWindowedMode(640*2, 480*2);
 		// config.width = (int)(640*1.0);
 		// config.height = (int)(480*1.0);
 
-		new LwjglApplication(new SpaceflyGame(), config);
+		new Lwjgl3Application(new SpaceflyGame(), config);
 
 	}
 }
--- desktop/src/com/vesas/spacefly/desktop/TestLauncher.java
+++ desktop/src/com/vesas/spacefly/desktop/TestLauncher.java
@@ -1,31 +1,32 @@
 package com.vesas.spacefly.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.vesas.spacefly.TestGame;
 
 public class TestLauncher {
 
 	
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		
-		config.title = "spacefly";
-		config.fullscreen = false;
-		config.vSyncEnabled = false;
-		config.foregroundFPS = 60;
+		config.setTitle("spacefly");
+		// config.fullscreen = false;
+		// config.vSyncEnabled = false;
+		// config.foregroundFPS = 60;
 		// config.width = 1100;
 		// config.height = 650;
 		// config.width = 640*6;
-		config.height = 480*4;
-		config.width = 640*4;
+		config.setWindowedMode(640*2, 480*2);
+		// config.height = 480*4;
+		// config.width = 640*4;
 		// config.height = 480*4;
 		// config.width = 640*2;
 		// config.height = 480*2;
 		// config.width = (int)(640*1.2);
 		// config.height = (int)(480*1.2);
 
-		new LwjglApplication(new TestGame(), config);
+		new Lwjgl3Application(new TestGame(), config);
 
 	}
 }
